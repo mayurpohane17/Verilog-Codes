79aee3acb737f99abee8665e07f706a780ac3f3a
Mayur Pohane <33730105+mayurpohane17@users.noreply.github.com>
2019-05-03 02:53:10 +0530

    Update AND_using_NAND.v

--- AND_using_NAND.v
+++ AND_using_NAND.v
@@ -16,18 +16,31 @@ NAND2gate N2 (.A(t1),.B(t1),.Z(Z));
 endmodule
 
 Testbench Code:-
-module NAND2gate(A,B,Z);
-input A,B;
-output Z;
-assign Z=~(A&B);
+module Main_tb;
+reg A;
+reg B;
+wire Z;
+
+AND2gate uut (.A(A),.B(B),.Z(Z));
+
+initial begin
+$dumpfile("test.vcd");
+    $dumpvars(0,Main_tb);
+
+$monitor(A,B,Z);
+
+A=1'b0;B=1'b0;
+#5
+A=1'b0;B=1'b1;
+#5
+A=1'b1;B=1'b0;
+#5
+A=1'b1;B=1'b1;
+
+$finish;
+
+end
+
 endmodule
 
-module AND2gate(A,B,Z);
 
-input A,B;
-output Z;
-wire t1;
-NAND2gate N1 (.A(A),.B(B),.Z(t1));
-NAND2gate N2 (.A(t1),.B(t1),.Z(Z));
-
-endmodule
