bb329a17a1dd99e906b85158fc5f35d35a8a9af4
Mayur Pohane <33730105+mayurpohane17@users.noreply.github.com>
2019-05-10 02:59:48 +0530

    Update BURGLAR_ALARM.v

--- BURGLAR_ALARM.v
+++ BURGLAR_ALARM.v
@@ -5,17 +5,15 @@ input SYS,WS,MD;
 output reg ALARM;
 
 always@(SYS,WS,MD)
-begin
-if(SYS==1'b1)
-begin
-
-if(WS==1'b1|MD==1'b1)
-assign ALARM=1'b1;
-else 
-  assign ALARM=1'b0;
-end
-else
-assign ALARM=1'b0;
-end
-
+  begin
+    if(SYS==1'b1)
+    begin
+         if(WS==1'b1|MD==1'b1)
+              assign ALARM=1'b1;
+         else 
+              assign ALARM=1'b0;
+    end
+    else
+        assign ALARM=1'b0;
+  end
 endmodule  
